ce86eee10a8cc564ca4f6a3a5b301ddc88907a70
AngelDevelop <angelpublicista.1@gmail.com>
2022-01-14 19:24:29 -0500

    final responsive

--- components/CarouselGen.jsx
+++ components/CarouselGen.jsx
@@ -2,7 +2,7 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
-import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+import { Navigation, Pagination, Scrollbar, A11y, Breakpoints } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 import ImgCarousel from "../public/logo-carrusel.png";
 import Image from "next/image";
@@ -16,6 +16,23 @@ export default function CarouselGen() {
       slidesPerView={5}
       onSwiper={(swiper) => console.log(swiper)}
       onSlideChange={() => console.log("slide change")}
+      breakpoints={{
+        375: {
+          width: 375,
+          slidesPerView: 1,
+          spaceBetween: 10,
+        },
+        768: {
+          width: 768,
+          slidesPerView: 2,
+          spaceBetween: 10,
+        },
+        1280: {
+          width: 1280,
+          slidesPerView: 6,
+          spaceBetween: 10,
+        },
+      }}
     >
       <SwiperSlide>
         <Image
