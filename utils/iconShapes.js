eaa6a0866d1736abee24dcbafcea133ab34f7b3e
Angeldevelop <you@example.com>
2022-03-30 19:44:15 -0500

    fix: restart utils folder

---
+++ utils/iconShapes.js
@@ -0,0 +1,6 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {fab, faFacebook, faInstagram, faLinkedin, faTwitter, faYoutube} from "@fortawesome/free-brands-svg-icons";
+
+export default function iconShapes(){
+    library.add(fab, faFacebook, faInstagram, faLinkedin, faTwitter, faYoutube)
+}
