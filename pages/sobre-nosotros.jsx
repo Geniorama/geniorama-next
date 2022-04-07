c36356ebf0cc7c794b6ef243f10170dedc3de7eb
Angeldevelop <you@example.com>
2022-04-07 09:55:40 -0500

    feature: nuevos testimonios

--- pages/sobre-nosotros.jsx
+++ pages/sobre-nosotros.jsx
@@ -15,14 +15,16 @@ import Testimonial from "../components/Testimonial";
 import AvatarExample from "../public/pexels-pixabay-415829.jpg";
 import SvgIconOurVission from "../components/SvgImages/SvgIconOurVission";
 import SvgIconOurMission from "../components/SvgImages/SvgIconOurMission";
-import DiegoRodriguez from "../public/testimonios/diego-rodriguez.jpg"
-import TaxoLogoAvatar from "../public/testimonios/taxo-logo-avatar.jpg"
-import CristianAaron from "../public/testimonios/cristian-aaron.jpg"
-import JuanArango from "../public/testimonios/juan-f-arango.jpg"
+import DiegoRodriguez from "../public/testimonios/diego-rodriguez.jpg";
+import TaxoLogoAvatar from "../public/testimonios/taxo-logo-avatar.jpg";
+import CristianAaron from "../public/testimonios/cristian-aaron.jpg";
+import JuanArango from "../public/testimonios/juan-f-arango.jpg";
+import SylviaRugeles from "../public/testimonios/sylvia-rugeles.jpg";
+import SorangieLopez from "../public/testimonios/sorangie-lopez.jpg";
 
 export default function SobreNosotros() {
   return (
-    <Layout title={"Servicios - Geniorama"} idPage={"gen-servicios"}>
+    <Layout title={"Nosotros - Geniorama"} idPage={"gen-nosotros"}>
       <div className="mt-5">
         <HeadingPage
           sTitle={"SOBRE NOSOTROS"}
@@ -34,16 +36,19 @@ export default function SobreNosotros() {
         <div className="container">
           <div className={styles.genWrapCardsAbout}>
             <CardAboutUs
-              icon={<SvgIconOurVission/>}
+              icon={<SvgIconOurVission />}
               title={"NUESTRA VISIÓN"}
-              description={"Nuestros objetivos están encaminados en ser la agencia de Marketing Digital y Desarrollo Web líder en el mercado colombiano y en un mediano plazo exportar servicios en mercados importantes como Estados Unidos y algunas regiones de Europa"}
+              description={
+                "Nuestros objetivos están encaminados en ser la agencia de Marketing Digital y Desarrollo Web líder en el mercado colombiano y en un mediano plazo exportar servicios en mercados importantes como Estados Unidos y algunas regiones de Europa"
+              }
             />
             <CardAboutUs
-              icon={<SvgIconOurMission/>}
+              icon={<SvgIconOurMission />}
               title={"NUESTRA MISIÓN"}
-              description={"Para llevar a cabo los proyectos contamos con personas apasionadas por su oficio y trabajamos bajo una metodología que permite a nuestros colaboradores tomar decisiones creativas sobre los procesos, garantizando así proyectos diferenciados y de calidad."}
+              description={
+                "Para llevar a cabo los proyectos contamos con personas apasionadas por su oficio y trabajamos bajo una metodología que permite a nuestros colaboradores tomar decisiones creativas sobre los procesos, garantizando así proyectos diferenciados y de calidad."
+              }
             />
-           
           </div>
         </div>
       </section>
@@ -65,7 +70,9 @@ export default function SobreNosotros() {
               image={ImgAngelBurgos}
               name="Angel Burgos"
               position={"CTO / Web Manager"}
-              linkedin={"https://www.linkedin.com/in/angel-burgos-mesa-a5907a126/"}
+              linkedin={
+                "https://www.linkedin.com/in/angel-burgos-mesa-a5907a126/"
+              }
               github={"https://github.com/angelpublicista"}
             />
 
@@ -87,33 +94,36 @@ export default function SobreNosotros() {
               name="Felipe Hernández"
               position={"UX/UI & Digital Designer"}
               linkedin={"https://www.linkedin.com/in/fyliip96/"}
-              behance={"https://www.behance.net/Fyliip96?tracking_source=search_users%7Cfelipe%20hernandez"}
+              behance={
+                "https://www.behance.net/Fyliip96?tracking_source=search_users%7Cfelipe%20hernandez"
+              }
             />
 
             <CardTeamGen
               image={ImgCristianM}
               name="Cristian Marín"
               position={"Content Manager & Creative"}
-              linkedin={"https://www.linkedin.com/in/cristhian-camilo-mar%C3%ADn-linares-7b976754/"}
+              linkedin={
+                "https://www.linkedin.com/in/cristhian-camilo-mar%C3%ADn-linares-7b976754/"
+              }
             />
 
             <CardTeamGen
               image={ImgDuvanR}
               name="Duvan Rodríguez"
               position={"CTO / Frontend Developer"}
-              linkedin={"https://www.linkedin.com/in/giovanni-rodr%C3%ADguez-25b9b621a/"}
+              linkedin={
+                "https://www.linkedin.com/in/giovanni-rodr%C3%ADguez-25b9b621a/"
+              }
               github={"https://github.com/DuvanProgrammer"}
             />
-            
           </div>
         </div>
       </section>
 
       <section className="gen-info-section">
         <div className="container">
-          <h2 className="gen-info-section__title">
-            PARTNERS
-          </h2>
+          <h2 className="gen-info-section__title">PARTNERS</h2>
           <div className={styles.genCarouselBrands}>
             <CarouselGen />
           </div>
@@ -129,7 +139,9 @@ export default function SobreNosotros() {
             <Testimonial
               image={TaxoLogoAvatar}
               name={"Lina Duque"}
-              position={"CEO <a href='http://tax-o.com.co/' target='_blank' rel='noreferrer'>Taxo</a>"}
+              position={
+                "CEO <a href='http://tax-o.com.co/' target='_blank' rel='noreferrer'>Taxo</a>"
+              }
               text={
                 "Con sus ideas logramos una mayor cantidad de interacciones en redes sociales y vamos por buen camino en el posicionamiento de marca con nuestros Blogs y Posts. Muchas Gracias!"
               }
@@ -138,16 +150,25 @@ export default function SobreNosotros() {
             <Testimonial
               image={DiegoRodriguez}
               name={"Diego Rodríguez"}
-              position={"Founder & Director <a href='https://ultravioleta.co/' target='_blank' rel='noreferrer'>Ultravioleta.co</a>"}
+              position={
+                "Founder & Director <a href='https://ultravioleta.co/' target='_blank' rel='noreferrer'>Ultravioleta.co</a>"
+              }
               text={
-                <iframe src="https://widget.spreaker.com/player?episode_id=49245846&theme=light&playlist=false&playlist-continuous=false&chapters-image=true&episode_image_position=right&hide-logo=false&hide-likes=false&hide-comments=false&hide-sharing=false&hide-download=true" width="100%" height="150px" frameBorder="0"></iframe>
+                <iframe
+                  src="https://widget.spreaker.com/player?episode_id=49245846&theme=light&playlist=false&playlist-continuous=false&chapters-image=true&episode_image_position=right&hide-logo=false&hide-likes=false&hide-comments=false&hide-sharing=false&hide-download=true"
+                  width="100%"
+                  height="150px"
+                  frameBorder="0"
+                ></iframe>
               }
             />
 
             <Testimonial
               image={CristianAaron}
               name={"Cristian Aaron"}
-              position={"Founder <a href='http://naranjaexport.com/' target='_blank' rel='noreferrer'>Naranja Export</a>"}
+              position={
+                "Founder <a href='http://naranjaexport.com/' target='_blank' rel='noreferrer'>Naranja Export</a>"
+              }
               text={
                 "Cambiamos nuestra imagen y nuestra página web para hacerla más atractiva y sólida para nuestros clientes. También empezamos a generar contenido para potenciar nuestras ventas. Excelente gestión"
               }
@@ -159,6 +180,28 @@ export default function SobreNosotros() {
               position={"Director Creativo Ejecutivo Independiente"}
               text={
                 "El equipo Geniorama se ha convertido en mi aliado estratégico. Desde nuestro primer proyecto me dieron la confianza y calidad para escogerlos con sus soluciones de desarrollo web y administración de campañas de pauta en redes sociales. Hoy ya tenemos una relación de negocio consolidada. ¡Un gran equipo!"
+              }
+            />
+
+            <Testimonial
+              image={SylviaRugeles}
+              name={"Sylvia Rugeles"}
+              position={
+                "Fundadora y Directora Ejecutiva <a href='https://www.socialthink.co/' target='_blank' rel='noreferrer'>Social Think</a>"
+              }
+              text={
+                "Genios para hacer webs y genios para tratar con los clientes. Genios por su profesionalismo y su capacidad de solucionar en cualquier momento. Genios por su calidez humana y su transparencia. En Geniorama hay genios integrales, profesionales y empáticos. Gracias!"
+              }
+            />
+
+            <Testimonial
+              image={SorangieLopez}
+              name={"Sorangie López"}
+              position={
+                "Socio Gerente en <a href='http://univercity.com.co/' target='_blank' rel='noreferrer'>UniverCity</a>"
+              }
+              text={
+                "Geniorama Agencia, es el mejor Partner en asuntos creativos,  diseño de estrategias digitales y programación web con la mejor experiencia e interfaz de usuario.  Su versatilidad 360, que incluye producción audiovisual de calidad y acciones enfocadas BTL, hace realidad el cumplimiento de ambiciosos objetivos de comunicación para nuestras marcas"
               }
             />
           </div>
