cb9048e66fa0715f1d7a83e21730573dfa6f1f2d
DiegoFreelance <diego.burgos.mesa95@gmail.com>
2022-06-28 09:06:15 -0500

    Descativar Popup

--- pages/index.jsx
+++ pages/index.jsx
@@ -37,7 +37,6 @@ import ImgMasDeNosotros from "../public/img/img-mas-de-nosotros.png"
 import ReCAPTCHA from 'react-google-recaptcha'
 import ImgHeaderPopup from '../public/img/header-popup.jpg'
 import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
-import PopupForm from "../components/PopupForm";
 
 export default function Home({data, dataBlog}) {
   useEffect(()=>{
@@ -46,11 +45,7 @@ export default function Home({data, dataBlog}) {
     if(!localStorage.getItem('isFormComplete')){
         const activeClass = styles.activePopup
         const showPopup = () => {
-          const popup = document.getElementById('pop-up')
-          popup.classList.add(activeClass)
         }
-
-        setTimeout(showPopup, 5000)
     }
 
     
@@ -61,22 +56,6 @@ export default function Home({data, dataBlog}) {
       idPage={"gen-home"}
       isLogoLight
       >
-      
-      <div className={styles.popupHome} id="pop-up">
-          <div className={styles.popupHomeBox}>
-              <div className={styles.popupHomeBox__header}>
-                  <Image src={ImgHeaderPopup} layout="responsive"/>
-              </div>
-              <div className={styles.popupHomeBox__body}>
-                <p className={styles.popupHomeBox__body__desc}>
-                  Déjanos tus datos y recibe un bono de <strong>30% de descuento</strong> en el desarrollo de tu tienda virtual
-                </p>
-
-                <PopupForm />
-              </div>
-              
-          </div>
-      </div>
 
       <BannerHome />
 
